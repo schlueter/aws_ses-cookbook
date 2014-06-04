b68b3cadf11334ed8145799c5d7ab359108ad715
Schlueter <bs@bschlueter.com>
2014-06-04 03:20:53 -0400

    Create initial resources w/recipe for aws-ses gem

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'ses-mailer'
+name             'amazon_ses'
 maintainer       'Schlueter'
 maintainer_email 'bs@bschlueter.com'
 license          'GPL v3'
-description      'Configures a system to send mail with Amazon SES, and sends it too'
+description      'Sends mail with Amazon SES'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
